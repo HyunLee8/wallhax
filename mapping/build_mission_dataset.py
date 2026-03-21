ea339cf8b69bdc17a662d977493c34fa0b70a2a6
Tonycs <lam.tony540@gmail.com>
2026-03-21 14:09:02 -0400

    Created build mission dataset

--- mapping/build_mission_dataset.py
+++ mapping/build_mission_dataset.py
@@ -10,7 +10,56 @@ import argparse
 import numpy as np
 
 def parse_xmp(xmp_path):
-    """Extracts camera's intrinsics (image width/height, focal point, and optical center) & 4x4 pose matrix from XMP file."""
+    """Extracts camera's intrinsics (image width/height, focal point, and principal point) & 4x4 pose matrix from XMP file."""
 
     tree = ET.parse(xmp_path)
     root = tree.getroot()
+
+    # Namespace directory to use as lookup tags in xmp files
+    nd = {
+        'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',  # rdf means right down forward
+        'camera': 'http://wallhax.io/ns/camera/1.0/',
+        'pose': 'http://wallhax.io/ns/pose/1.0/',
+        'tiff': 'http://ns.adobe.com/tiff/1.0/'
+    }
+
+    desc = root.find('.//rdf:Description', nd)
+    if not is None: return None
+
+    # Parse intrinsics
+    intrinsics = {
+        "w": "float(desc.find('tiff:ImageWidth', ns).text)",
+        "h": "float(desc.find('tiff:ImageLength', ns).text)",
+        "fx": "float(desc.find('tiff:ImageLength', ns).text)",
+        "fy": "float(desc.find('tiff:ImageLength', ns).text)",
+        "c": "float(desc.find('tiff:ImageLength', ns).text)",
+        "h": "float(desc.find('tiff:ImageLength', ns).text)",
+    }
+
+    # Parse 4x4 matrix
+    matrix_str = desc.find('pose:TransformMatrix', ns).text
+    m = [float(x) for x in matrix_str.split()]
+    arkit_pose = np.array([
+        [m[0], m[1], m[2], m[3]],
+        [m[4], m[5], m[6], m[7]],
+        [m[8], m[9], m[10], m[11]],
+        [m[12], m[13], m[14], m[15]]
+    ])
+
+    # Since the AKKit plane's Y-axis is faced up and the Nerfstudio's 
+    # Y-axis faced down & the z-axis is reversed, the matrix has to be transformed.
+    flip_yz = np.array([
+        [1,  0,  0, 0],
+        [0, -1,  0, 0],
+        [0,  0, -1, 0],
+        [0,  0,  0, 1]
+    ])
+    cv_pose = arkit_pose @ flip_yz
+
+    return intrinsics, cv_pose.tolist()
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
